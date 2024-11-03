87476ce75f63dc50f3bf24994225de4eea0f7c9c
Joe Maliksi <jmaliksi@users.noreply.github.com>
2024-11-03 14:26:30 -0500

    40 percent

--- onomancer/database.py
+++ onomancer/database.py
@@ -247,7 +247,7 @@ def get_leaders(top=20):
 def get_random_name():
     conn = connect()
     with connect() as conn, debug_log() as log:
-        if random.random() > .35:
+        if random.random() > .4:
             log['mode'] = 'eggs'
             lower = conn.execute(
                 f'''
